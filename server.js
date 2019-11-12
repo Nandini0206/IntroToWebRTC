fba0f86dac6c344ccfc3ea5b5a71e584c4e2ec76
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-11-12 11:20:20 +0000

    changing video styling

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 console.log('server started');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
   res.render('index.ejs');
 });
